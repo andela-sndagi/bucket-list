3e0a231baac252081203b2131ef2cf8121c322b0
ndagistanley <stanley.ndagi@andela.com>
2016-02-09 16:15:16 +0300

    Before peewee hard-on course

--- app.py
+++ app.py
@@ -1,18 +1,12 @@
-import os, sys
-import inspect
-currentdir = os.path.dirname(os.path.abspath(
-    inspect.getfile(inspect.currentframe())))
-parentdir = os.path.dirname(currentdir)
-sys.path.insert(0, parentdir)
-
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, jsonify, request, abort
 from models import *
 
-app = Flask(__name__)
+
+app = Flask(__name__) # Initialise Flask
 
 @app.before_request
 def before_request():
-    # create db if needed and connet
+    # create db if needed and connect
     initialize_db()
 
 @app.teardown_request
@@ -20,25 +14,154 @@ def teardown_request(exception):
     # close the db connection
     db.close()
 
-@app.route('/')
-def home():
-    # render the home page with the saved Bucketlists
-    return render_template('home.html', bucketlists=Bucketlist.select().order_by(Bucketlist.date_created.desc()))
+# @app.route('/auth/login', methods = ['POST'])
+# def login():
+#     token = [
+#         {
+#             "token": "t0k3n"
+#         }
+#     ]
+#     return jsonify({'token': token})
 
-@app.route('/new_bucketlist/')
-def new_bucketlist():
-    return render_template('new_bucketlist.html')
+# @app.route('/auth/register', methods = ['POST'])
+# def register():
+#     return 'POST'
 
-@app.route('/create/', methods=['POST'])
+"""
+Bucket List Collection [/bucketlists/]
+"""
+
+bucketlists = [
+    {
+        "id": 1,
+        "name": "BucketList1",
+    },
+    {
+        "id": 2,
+        "name": "BucketList2",
+    },
+    {
+        "id": 3,
+        "name": "BucketList3",
+    }
+]
+
+@app.route('/bucketlists', methods = (['GET']))
+def get_bucketlists():
+    return jsonify({'bucketlists': bucketlists})
+
+@app.route('/bucketlists', methods = (['POST']))
 def create_bucketlist():
-    # create the new Bucketlist
-    Bucketlist.create(
-        name = request.form['title'],
-        created_by = request.form['text']
-    )
+    # bucketlist = Bucketlist.create(name = request.json['name'])
+    # bucketlist.save()
+    # return jsonify({'bucketlist': bucketlist}), 201
+    if not request.json or not 'name' in request.json:
+        abort(400)
+    bucketlist = {
+        'id': bucketlists[-1]['id'] + 1,
+        'name': request.json['name'],
+    }
+    bucketlists.append(bucketlist)
+    return jsonify({'bucketlist': bucketlist}), 201
 
-    # return the user to the home page
-    return redirect(url_for('home'))
+"""
+Single Bucketlist [/bucketlists/<id>]
+"""
+
+items = [
+    {
+        "id": 1,
+        "name": "I need to do X",
+        "date_created": "2015-08-12 11:57:23",
+        "date_modified": "2015-08-12 11:57:23",
+        "done": False
+    }
+]
+
+@app.route('/bucketlists/<id>', methods = (['GET']))
+def get_single_bucketlist(id):
+    return jsonify({'items': items})
+
+
+@app.route('/bucketlists/<int:id>', methods = (['PUT']))
+def update_bucketlist(id):
+    item = [item for item in items if item['id'] == id]
+    # if len(item) == 0:
+    #     abort(404)
+    # if not request.json:
+    #     abort(400)
+    # if 'name' in request.json and type(request.json['name']) != unicode:
+    #     abort(400)
+    # if 'date_modified' in request.json and type(request.json['date_modified']) is not unicode:
+    #     abort(400)
+    # if 'done' in request.json and type(request.json['done']) is not bool:
+    #     abort(400)
+    item[0]['name'] = request.json['name']
+    item[0]['date_modified'] = request.json['date_modified']
+    item[0]['done'] = request.json['done']
+    return jsonify({'item': item[0]})
+
+@app.route('/bucketlists/<int:id>', methods = (['DELETE']))
+def delete_bucketlist(id):
+    item = [item for item in items if item['id'] == id]
+    if len(item) == 0:
+        abort(404)
+    items.remove(item[0])
+    return jsonify({'result': True})
+
+# def get_task(task_id):
+#     task = [task for task in tasks if task['id'] == task_id]
+#     if len(task) == 0:
+#         abort(404)
+#     return jsonify({'task': task[0]})
+
+# def specificbl(id):
+#     if request.method == 'GET':
+#         return jsonify({'items': items})
+#     elif request.method == 'PUT':
+#         item = [item for item in items if item['id'] == item_id]
+#         if len(item) == 0:
+#             abort(404)
+#         if not request.json:
+#             abort(400)
+#         if 'name' in request.json and type(request.json['name']) != unicode:
+#             abort(400)
+#         if 'date_modified' in request.json and type(request.json['date_modified']) is not unicode:
+#             abort(400)
+#         if 'done' in request.json and type(request.json['done']) is not bool:
+#             abort(400)
+#         item[0]['name'] = request.json.get('name', item[0]['name'])
+#         item[0]['date_modified'] = request.json.get('date_modified', task[0]['date_modified'])
+#         item[0]['done'] = request.json.get('done', item[0]['done'])
+#         return jsonify({'item': item[0]})
+#     elif request.method == 'DELETE':
+#         task = [item for item in items if item['id'] == item_id]
+#         if len(item) == 0:
+#             abort(404)
+#         items.remove(item[0])
+#         return jsonify({'result': True})
+
+
+
+# Single Item in a Bucketlist [/bucketlists/<id>/items/<item_id>]
+
+@app.route('/bucketlists/<id>/items/', methods = ['POST'])
+def blitems(id):
+    if not request.json or not 'title' in request.json:
+        abort(400)
+        item = {
+            "item_id": 2,
+            "name": "I need to do Y",
+            "date_created": "2015-08-12 11:59:23",
+            "date_modified": "2015-08-12 11:59:23",
+            "done": True
+        }
+        items.append(item)
+        return jsonify({'item': item}), 201
+
+# @app.route('/bucketlists/<id>/items/<item_id>', methods = (['PUT'], ['DELETE']))
+# def bliitem(id, item_id):
+#     pass
 
 if __name__ == '__main__':
     app.run(debug=True)
--- models.py
+++ models.py
@@ -1,20 +1,38 @@
-from peewee import *
+from peewee import * # ORM
 import datetime
 
-db = SqliteDatabase('bucketlists.db')
+db = SqliteDatabase('bucketlist.db')
 
 class Bucketlist(Model):
+    """
+    Model for Bucketlist Table
+    """
+
     id = PrimaryKeyField()
     name = CharField()
     date_created = DateTimeField(default = datetime.datetime.now)
     date_modified = DateTimeField(default = datetime.datetime.now)
     created_by = CharField()
 
+    class Meta:
+        database = db
+
+
+class BucketlistItem(Model):
+    """
+    Model for BucketlistItem Table
+    """
+    id = ForeignKeyField(Bucketlist, related_name='items')
+    item_id = PrimaryKeyField()
+    name = CharField()
+    date_created = DateTimeField(default = datetime.datetime.now)
+    date_modified = DateTimeField(default = datetime.datetime.now)
+    done = BooleanField()
 
     class Meta:
         database = db
 
+
 def initialize_db():
     db.connect()
-    db.create_tables([Bucketlist], safe=True)
-
+    db.create_tables([Bucketlist, BucketlistItem], safe=True)
