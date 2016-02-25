3342fcaa10c704c0cf170824abef4f6f1d4cf85c
Stanley Ndagi <stanley.ndagi@andela.com>
2016-02-25 16:10:05 +0300

    (fix) Test error

--- tests.py
+++ tests.py
@@ -5,17 +5,16 @@ import app
 class AppTestCase(unittest.TestCase):
     """docstring for AppTestCase"""
     def setUp(self):
-    """Setting up the environment for testing"""
+        """Setting up the environment for testing"""
         app.app.config['TESTING'] = True
         self.app = app.app.test_client()
 
-    @staticmethod
     def tearDown(self):
-    """Tearing down after tests"""
+        """Tearing down after tests"""
         print('=> Tearing down after tests')
 
     def test_api_index(self):
-    """Test if the index page is working"""
+        """Test if the index page is working"""
         resp = self.app.get('/')
         assert "200 OK" == resp.status
         assert "Bucket List Service API is ready" in resp.data
